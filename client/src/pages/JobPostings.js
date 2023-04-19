b8b3401943bb8e57ef3f00f40e2cf803cb386570
damaomao84 <13324153861@163.com>
2023-04-19 17:34:40 +0800

    client: job-postings page

---
+++ client/src/pages/JobPostings.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+const JobPostings = () => {
+    return (
+        <>
+            <section id="job_postings" className="section">
+                <div className="container">
+                    <img src="./images/pages/job-postings/job-postings-top.png" alt="" />
+                    <Link to={-1} className="link">
+                        <span className='breadcrumb'>zurück</span>
+                    </Link>
+                    <h1 className='job_postings_title'>GC Pharma GmbH als Arbeitgeber</h1>
+                    <div className='job_postings_rectangle'></div>
+                    <p className='job_postings_description'>Unser Mindset, Veränderungsprozessen unvoreingenommen und mutig zu begegnen, hat sich ebenso wenig verändert, wie die zentrale Verankerung des Erfolgsfaktors „Mensch“ in unserem unternehmerischen Bewusstsein. Unsere Mitarbeiter:innen sind nicht nur wegen ihrer Cleverness stets das Wichtigste unseres Unternehmens. Was die Menschen in der GC Pharma GmbH auszeichnet, ist eine unnachahmliche Mischung aus Intuition, Freude und Begeisterung.
+                    </p>
+                    <Link to="/">
+                        <button type="button" className="base_button main_button">58 STELLEN ERKUNDEN</button>
+                    </Link>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default JobPostings
