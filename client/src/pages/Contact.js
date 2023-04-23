ed092ead1bcb03eb37a2e8e288f24b97e15d3721
damaomao84 <13324153861@163.com>
2023-04-23 19:14:35 +0800

    client: contact-page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -15,6 +15,15 @@ const Contact = () => {
                     <p className='contact_description'>Unser Mindset, Veränderungsprozessen unvoreingenommen und mutig zu begegnen, hat sich ebenso wenig verändert, wie die zentrale Verankerung des Erfolgsfaktors „Mensch“ in unserem unternehmerischen Bewusstsein. Unsere Mitarbeiter:innen sind nicht nur wegen ihrer Cleverness stets das Wichtigste unseres Unternehmens. Was die Menschen in der GC Pharma GmbH auszeichnet, ist eine unnachahmliche Mischung aus Intuition, Freude und Begeisterung.
                     </p>
 
+                    <div className="services_card">
+                        <div className="services_icon"><img src="/images/pages/contact/position.png" alt="" /></div>
+                        <div>ZENTRALE</div>
+                        <div className="services_details">
+                        <h4>German Capital Pharma GmbH</h4>
+                        <p>Badstr. 20 13357 Berlin, Deutschland</p>
+                        </div>
+                    </div>
+
                     <CustomDropdown name='fruit' options={[
                         {name: 'Arzneimittelsicherheit', value: 'Arzneimittelsicherheit'},
                         {name: 'Biometrie', value: 'Biometrie', defaultValue: true},
@@ -22,15 +31,7 @@ const Contact = () => {
                         {name: 'Beratung', value: 'Beratung'}
                     ]} onChange={(e) => {console.log(e.target.value)}} style={{width: '20em'}} />
 
-                    <div>
-                        asdfasdfasdfasdfasdfasdf<br />
-                        asdfasdfasdfasdfasdfasdf<br />
-                        asdfasdfasdfasdfasdfasdf<br />
-                        asdfasdfasdfasdfasdfasdf<br />
-                        asdfasdfasdfasdfasdfasdf<br />
-                        asdfasdfasdfasdfasdfasdf<br />
-                        asdfasdfasdfasdfasdfasdf<br />
-                    </div>
+                    
                 </div>
             </section>
         </>
