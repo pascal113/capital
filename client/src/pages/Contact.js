658a40fb4899c1c4a2b57f28599a2bae6a3848d3
damaomao84 <13324153861@163.com>
2023-04-23 23:48:33 +0800

    client: contact-page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from "react-router-dom";
 import BreadCrumb from '../components/common/BreadCrumb';
 import CustomDropdown from '../components/dropdown/CustomDropdown';
 
 const Contact = () => {
+    const [status, setStatus] = useState("Submit");
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setStatus("Sending...");
+        const { name, email, message } = e.target.elements;
+        let details = {
+        name: name.value,
+        email: email.value,
+        message: message.value,
+        };
+        let response = await fetch("http://localhost:5000/contact", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json;charset=utf-8",
+        },
+        body: JSON.stringify(details),
+        });
+        setStatus("Submit");
+        let result = await response.json();
+        alert(result.status);
+    };
+
     return (
         <>
             <section id="contact" className="section">
@@ -12,24 +34,55 @@ const Contact = () => {
                     <BreadCrumb />
                     <h1 className='contact_title'>Sie haben Fragen?</h1>
                     <div className='contact_rectangle'></div>
-                    <p className='contact_description'>Unser Mindset, Veränderungsprozessen unvoreingenommen und mutig zu begegnen, hat sich ebenso wenig verändert, wie die zentrale Verankerung des Erfolgsfaktors „Mensch“ in unserem unternehmerischen Bewusstsein. Unsere Mitarbeiter:innen sind nicht nur wegen ihrer Cleverness stets das Wichtigste unseres Unternehmens. Was die Menschen in der GC Pharma GmbH auszeichnet, ist eine unnachahmliche Mischung aus Intuition, Freude und Begeisterung.
-                    </p>
-
-                    <div className="services_card">
-                        <div className="services_icon"><img src="/images/pages/contact/position.png" alt="" /></div>
-                        <div>ZENTRALE</div>
-                        <div className="services_details">
-                        <h4>German Capital Pharma GmbH</h4>
-                        <p>Badstr. 20 13357 Berlin, Deutschland</p>
+                    <div className="contact_position">
+                        <div className="position_icon"><img src="/images/pages/contact/position.png" alt="" /></div>
+                        <div className='headquarters'>ZENTRALE</div>
+                        <div className="position_address">
+                            <h4>German Capital Pharma GmbH</h4>
+                            <p>Badstr. 20 13357 Berlin, Deutschland</p>
                         </div>
+                        
+                    </div>
+                    <div className='contact_form_wrapper'>
+                        <form className='contact_form'onSubmit={handleSubmit} method="POST">
+                            <div className='form_title'><span>Nehmen Sie Kontakt mit uns auf</span></div>
+                            <div className='form_comment'><span>Mit * markierte Felder sind Pflichtfelder.</span></div>
+                            <div className='form_department'>
+                                <span className="label">An welche Abteilung möchten Sie sich wenden?</span>
+                                <div className='dropdown'>
+                                    <CustomDropdown name='fruit' options={[
+                                    {name: 'Arzneimittelsicherheit', value: 'Arzneimittelsicherheit'},
+                                    {name: 'Biometrie', value: 'Biometrie', defaultValue: true},
+                                    {name: 'Info-Center', value: 'Info-Center'},
+                                    {name: 'Beratung', value: 'Beratung'}
+                                    ]} onChange={(e) => {console.log(e.target.value)}} style={{width: '191px'}} />
+                                </div>
+                            </div>
+                            <div className='form_company'>
+                                <div className='name'>
+                                    
+                                </div>
+                                <div className='telephone'>
+                                    
+                                </div>
+                            </div>
+                            <div className='form_message'>
+                                <label htmlFor="message">Message:</label>
+                                <textarea id="message" required />
+                            </div>
+                            <button type="submit">asdf</button>
+                        </form>
+                            
+                            
+                            
+                            
+                        
+                        <div className='contact_map'>
+                            <img src="/images/pages/contact/map.png" alt="" />
+                        </div>   
                     </div>
 
-                    <CustomDropdown name='fruit' options={[
-                        {name: 'Arzneimittelsicherheit', value: 'Arzneimittelsicherheit'},
-                        {name: 'Biometrie', value: 'Biometrie', defaultValue: true},
-                        {name: 'Info-Center', value: 'Info-Center'},
-                        {name: 'Beratung', value: 'Beratung'}
-                    ]} onChange={(e) => {console.log(e.target.value)}} style={{width: '20em'}} />
+                    
 
                     
                 </div>
