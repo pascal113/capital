9a2e2e0212a3be3002895fa434ca3d033c23684f
Your Name <you@example.com>
2023-04-23 22:58:47 -0400

    remove admin1

--- admin1/src/constants/SelectFields/SirCode.ts
+++
@@ -1,4 +0,0 @@
-export default [
-    { id: 'Mr', name: 'resources.users.constants.select_fields.name_sir_code.mr' },
-    { id: 'Mrs', name: 'resources.users.constants.select_fields.name_sir_code.mrs' },
-];
--- admin1/src/notifications/index.ts
+++
@@ -1,9 +0,0 @@
-import AddAlertIcon from '@mui/icons-material/AddAlert';
-import NotificationList from './notifications.list';
-
-const resource = {
-    list: NotificationList,
-    icon: AddAlertIcon
-}
-
-export default resource;
