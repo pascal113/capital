5a5964fc052ea0268d21aface378b13404ddb7da
damaomao84 <13324153861@163.com>
2023-05-01 16:36:37 +0800

    client-loading-page

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { CommonProvider } from './contexts/common/commonContext';
 import { MenuProvider } from './contexts/menu/menuContext';
 import Header from './components/common/Header';
@@ -7,11 +7,28 @@ import Footer from './components/common/Footer';
 import BackTop from './components/common/BackTop';
 import muiTheme from './components/theme/MuiTheme';
 import { ThemeProvider} from "@mui/material";
+import Preloader from './components/common/Preloader';
 
 const App = () => {
 
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    /*setTimeout(() => {
+      fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
+          setData(json);
+          setLoading(false);
+        });
+    }, 2000);*/
+    setTimeout(() => setLoading(false), 5000)
+  }, [])
+
   return (
     <>
+      {loading === true ? (<Preloader />) :(
       <CommonProvider>
         <Header />
         <MenuProvider>
@@ -22,6 +39,7 @@ const App = () => {
         <Footer />
         <BackTop />
       </CommonProvider>
+      )}
     </>
   );
 };
--- client/src/components/common/Preloader.js
+++ client/src/components/common/Preloader.js
@@ -1,37 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import useScrollDisable from '../../hooks/useScrollDisable';
-
+import React from 'react';
 
 const Preloader = () => {
-    const [isLoading, setIsLoading] = useState(true);
-
-    useScrollDisable(isLoading);
-
-    useEffect(() => {
-        const handleIsLoading = () => {
-            setIsLoading(false);
-        };
-
-        window.addEventListener('load', handleIsLoading);
-
-        return () => {
-            window.removeEventListener('load', handleIsLoading);
-        };
-    }, []);
-
 
     return (
-        <>
-            {
-                isLoading && (
-                    <div id="preloader">
-                        <div className="loader"></div>
-                        <span className="loader_text">Loading...</span>
+        <div id="pre-load"> 
+            <div id="loader" class="loader">
+                <div class="loader-container">
+                    <div class="loader-icon">
+                        <img src="./images/loading/logo.svg" height="40" alt="Initial loading..." />
                     </div>
-                )
-
-            }
-        </>
+                    <div className='loader-label'>Wird geladen...</div>
+                </div>
+                
+            </div>
+        </div>
     );
 };
 
