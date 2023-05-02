3795aba61ab52e95647dbd3d84cce498f2a3a196
Your Name <you@example.com>
2023-05-02 06:36:15 -0400

    change seed

--- server/seed/images.js
+++ server/seed/images.js
@@ -1,104 +1,133 @@
 const images = [
   {
-    path: 'banner1.png',
+    path: 'uploads/banner1.png',
     type: 'banner',
     index: 1,
     title: '',
     description: `banner image`
   },
   {
-    path: 'banner2.png',
+    path: 'uploads/banner2.png',
     type: 'banner',
     index: 2,
     title: '',
     description: `banner image`
   },
   {
-    path: 'banner3.png',
+    path: 'uploads/banner3.png',
     type: 'banner',
     index: 3,
     title: '',
     description: `banner image`
   },
   {
-    path: 'banner4.png',
+    path: 'uploads/banner4.png',
     type: 'banner',
     index: 4,
     title: '',
     description: `banner image`
   },
   {
-    path: 'banner5.png',
+    path: 'uploads/banner5.png',
     type: 'banner',
     index: 5,
     title: '',
     description: `banner image`
   },
   {
-    path: 'banner6.png',
+    path: 'uploads/banner6.png',
     type: 'banner',
     index: 6,
     title: '',
     description: `banner image`
   },
   {
-    path: 'banner7.png',
+    path: 'uploads/banner7.png',
     type: 'banner',
     index: 7,
     title: '',
     description: `banner image`
   },
   {
-    path: 'hamburger1.png',
+    path: 'uploads/hamburger1.png',
     type: 'hamburger',
     index: 1,
+    sub_index: 1,
     title: '',
     description: `hamburgermenu image`
   },
   {
-    path: 'hamburger2.png',
+    path: 'uploads/hamburger2.png',
     type: 'hamburger',
     index: 2,
     title: '',
     description: `hamburgermenu image`
   },
   {
-    path: 'hamburger3.png',
+    path: 'uploads/hamburger3.png',
     type: 'hamburger',
     index: 3,
     title: '',
     description: `hamburgermenu image`
   },
   {
-    path: 'hamburger4.png',
+    path: 'uploads/hamburger4.png',
     type: 'hamburger',
     index: 4,
     title: '',
     description: `hamburgermenu image`
   },
   {
-    path: 'hamburger5.png',
+    path: 'uploads/hamburger5.png',
     type: 'hamburger',
     index: 5,
     title: '',
     description: `hamburgermenu image`
   },
   {
-    path: 'hamburger6.png',
+    path: 'uploads/hamburger6.png',
     type: 'hamburger',
     index: 6,
     title: '',
     description: `hamburgermenu image`
   },
   {
-    path: 'hamburger7.png',
+    path: 'uploads/hamburger7.png',
     type: 'hamburger',
     index: 7,
     title: '',
     description: `hamburgermenu image`
   },
   {
-    path: 'hamburger8.png',
+    path: 'uploads/hamburger8.png',
+    type: 'hamburger',
+    index: 8,
+    title: '',
+    description: `hamburgermenu image`
+  },
+  {
+    path: 'uploads/hamburger9.png',
+    type: 'hamburger',
+    index: 8,
+    title: '',
+    description: `hamburgermenu image`
+  },
+  {
+    path: 'uploads/hamburger10.png',
+    type: 'hamburger',
+    index: 8,
+    title: '',
+    description: `hamburgermenu image`
+  },
+  {
+    path: 'uploads/hamburger11.png',
+    type: 'hamburger',
+    index: 8,
+    title: '',
+    description: `hamburgermenu image`
+  },
+  {
+    path: 'uploads/hamburger11.png',
     type: 'hamburger',
     index: 8,
     title: '',
