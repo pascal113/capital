b94b233813cc11782bb277a1c4fb2d89ed1d8bf9
damaomao84 <13324153861@163.com>
2023-05-05 00:01:42 +0800

    client-i18n-apply-page

--- client/src/pages/ApplyPage.js
+++ client/src/pages/ApplyPage.js
@@ -3,14 +3,14 @@ import BreadCrumb from '../components/common/BreadCrumb';
 import { useParams, useNavigate } from "react-router-dom";
 import ImageViewer from '../components/image/ImageViewer';
 import SelectUploadFile from '../components/upload/SelectUploadFile';
+import { useTranslation } from 'react-i18next';
 
 const ApplyPage = () => {
     const { id } = useParams();
     const id_num = parseInt(id);
     const navigate = useNavigate();
-    let clearChildState = null;
-    const cur_sel_files = ["Keine Datei ausgewählt", "Keine Datei ausgewählt", "Keine Datei ausgewählt"];
-    
+    const { t }  = useTranslation(['page']);
+        
     const imgViwerData = {
         img: "/images/pages/about-us/about-company-top.png",
         label: "People are the power to success",
@@ -20,17 +20,8 @@ const ApplyPage = () => {
         textColor: 'white',
     };
 
-    const assignClearChildState = (childClearStateFunc) =>{
-        clearChildState = childClearStateFunc;
-    };
-
-    const getStateCallback = (fileName) => {
-        //do something to use the child state
-    };
-
     const handleRest = e => {
         //clearChildState(); 
-        alert('asdfasdf');
     };
 
     const handleSubmit = async (e) => {
@@ -53,91 +44,88 @@ const ApplyPage = () => {
                     <ImageViewer data={imgViwerData} />
                     <BreadCrumb />
                     <div className='apply_wrapper'>
-                        <div className='title'><h1>HUMAN RECOURCES MANAGER  (VOLLZEIT)</h1></div>
-                        <div className='comment'><span >Bitte erfasse im Folgenden deine persönlichen Informationen.</span></div>
+                        <div className='title'><h1>{t('apply.title')}</h1></div>
+                        <div className='comment'><span >{t('apply.comment')}</span></div>
                         <form className='apply_form' onSubmit={handleSubmit} method="POST">
                             <div className='form_control'>
-                                <span className='label'>VORNAME*:</span>
-                                <input name="firstname" type="text" placeholder="Pflichtangabe" required />
+                                <span className='label'>{t('apply.firstname_label')}</span>
+                                <input name="firstname" type="text" placeholder={t('apply.mandatory_placeholder')} required />
                             </div>
                             <div className='form_control'>
-                                <span className='label'>NACHNAME*:</span>
-                                <input name="lastname" type="text" placeholder="Pflichtangabe" required />
+                                <span className='label'>{t('apply.lastname_label')}</span>
+                                <input name="lastname" type="text" placeholder={t('apply.mandatory_placeholder')} required />
                             </div>
                             <div className='form_control'>
-                                <span className='label'>E-MAIL LOGIN*:</span>
-                                <input name="email" type="email" placeholder="Pflichtangabe" required />
-                                <span className='label_comment'>Gib hier deine E-Mail Adresse ein.</span>
+                                <span className='label'>{t('apply.email_label')}</span>
+                                <input name="email" type="email" placeholder={t('apply.mandatory_placeholder')} required />
+                                <span className='label_comment'>{t('apply.email_comment')}</span>
                             </div>
                             <div className='form_control'>
-                                <span className='label'>TELEFONNUMMER*:</span>
-                                <input name="phone" type="tel" placeholder="Pflichtangabe" required pattern="^[ +0-9]*$"/>
-                                <span className='label_comment'>Bitte hinterlege deine Telefonnummer in folgendem Format, z.B. +49 170 1234567 (als Bsp. für eine deutsche Mobilfunknummer).</span>
+                                <span className='label'>{t('apply.phone_label')}</span>
+                                <input name="phone" type="tel" placeholder={t('apply.mandatory_placeholder')} required pattern="^[ +0-9]*$"/>
+                                <span className='label_comment'>{t('apply.phone_comment')}</span>
                             </div>
 
                             <div className='form_control'>
                                 <div className='apply-upload-section'>
                                     <div className='apply-upload-label'>
-                                        <span className='label'>DEIN ANSCHREIBEN/</span>
-                                        <span className='label'>LEBENSLAUF*:</span>
-                                    </div>
-                                    <SelectUploadFile getState={getStateCallback} passClearStateFunc={handleRest}/>
-                                </div>
-                                <div className='apply-upload-section'>
-                                    <div className='apply-upload-label'>
-                                        <span className='label'>DEINE ZEUGNISSE*:</span>
-                                        <span className='label-small'>ZERTIFIKATE/ SCHULZEUGNISSE</span>
+                                        <span className='label'>{t('apply.cv_label_top')}</span>
+                                        <span className='label'>{t('apply.cv_label_bottom')}</span>
                                     </div>
                                     <SelectUploadFile />
                                 </div>
                                 <div className='apply-upload-section'>
-                                    <span className='label'>WEITERE DOKUMENTE*:</span>
+                                    <div className='apply-upload-label'>
+                                        <span className='label'>{t('apply.reports_label_top')}</span>
+                                        <span className='label-small'>{t('apply.reports_label_bottom')}</span>
+                                    </div>
+                                    <SelectUploadFile />
+                                </div>
+                                <div className='apply-upload-section'>
+                                    <span className='label'>{t('apply.other_doc_label')}</span>
                                     <SelectUploadFile />
                                 </div>
                                 
                             </div>
 
                             <div className='form_control'>
-                                <span className='label'>DEIN FRÜHSTMÖGLICHER EINTRITTSTERMIN*:</span>
-                                <input name="entry_date" type="text" placeholder="Pflichtangabe" required pattern="^(1[0-2]|0?[1-9])/(3[01]|[12][0-9]|0?[1-9])/(?:(?:19|20)[0-9]{2})$"/>
-                                <span className='label_comment'>Bitte hinterlege deine eintrittstermin in folgendem Format, z.B. 1/25/2023</span>
+                                <span className='label'>{t('apply.entry_date_label')}</span>
+                                <input name="entry_date" type="text" placeholder={t('apply.phone_comment')} required pattern="^(1[0-2]|0?[1-9])/(3[01]|[12][0-9]|0?[1-9])/(?:(?:19|20)[0-9]{2})$"/>
+                                <span className='label_comment'>{t('apply.entry_date_comment')}</span>
                             </div>
                             <div className='form_control'>
-                                <span className='label'>DEINE GEHALTSVORSTELLUNGEN:</span>
-                                <input name="salary" type="text" placeholder="(optional)" />
+                                <span className='label'>{t('apply.salary_label')}</span>
+                                <input name="salary" type="text" placeholder={t('apply.optional_placeholder')} />
                             </div>
                             <div className='form_control'>
-                                <span className='label'>WIE BIST DU AUF UNS AUFMERKSAM GEWORDEN:</span>
-                                <input name="aware" type="text" placeholder="(optional)" />
+                                <span className='label'>{t('apply.aware_label')}</span>
+                                <input name="aware" type="text" placeholder={t('apply.optional_placeholder')} />
                             </div>
                             <div className='form_control'>
-                                <span className='label'>WEITERGABE DEINER BEWERBUNG*:</span>
+                                <span className='label'>{t('apply.transfer_apply_label')}</span>
                                 <div className='radio-box-section'>
                                     <input type="radio" name="radio-box" id="radio-box" className="custom-radio"/>
-                                    <span className='radio-label'>Ja</span>
-                                    <span className='radio-comment'>Ich bin damit einverstanden, dass meine Bewerbung zur Prüfung für weitere Stellenangebote an andere Fachbereiche bzw. Haufe-Gesellschaften übermittelt wird (dies gilt ausschl. für die vier Monate nach Abschluss meines Bewerbungsverfahrens). Diese Einwilligung kann ich jederzeit ohne Angabe von Gründen mit Wirkung für die Zukunft widerrufen.
-                                    </span>
+                                    <span className='radio-label'>{t('apply.yes')}</span>
+                                    <span className='radio-comment'>{t('apply.transfer_apply_agree')}</span>
                                 </div>
                                 <div className='radio-box-section'>
                                     <input type="radio" name="radio-box" id="radio-box" className="custom-radio"/>
-                                    <span className='radio-label'>Nein</span>
-                                    <span className='radio-comment'>Ich bin nicht damit einverstanden, dass meine Bewerbung zur Prüfung für weitere Stellenangebote an andere Fachbereiche bzw. Haufe-Gesellschaften übermittelt wird. Das bedeutet, dass meine Bewerbung nur von dem Fachbereich bzw. der Haufe-Gesellschaft berücksichtigt wird, die für dieses Stellenangebot zuständig ist.
-                                    </span>
+                                    <span className='radio-label'>{t('apply.no')}</span>
+                                    <span className='radio-comment'>{t('apply.transfer_apply_disagree')}</span>
                                 </div>
                             </div>
                             <div className='form_control agree-policy'>
-                                <span className='label'>Bitte bestätigen*:</span>
-                                <a target="_blank" href="https://www.gc-pharma.de/de/datenschutzerklaerung.html" className='link-apply'>Zur Datenschutzerklärung</a>
+                                <span className='label'>{t('apply.confirm_label')}</span>
+                                <a target="_blank" href="https://www.gc-pharma.de/de/datenschutzerklaerung.html" className='link-apply'>{t('apply.privacy_link')}</a>
                                 <div className='check-box-section'>
                                     <input type="checkbox" name="check-box" id="check-box" className="custom-check" />
-                                    <span className='check-label'>Ich bin mit der Datenschutzerklärung einverstanden</span>
+                                    <span className='check-label'>{t('apply.privacy_agree_label')}</span>
                                 </div>        
                             </div>
                             <div className='apply-button-range'>
-                                <button type="reset" className="apply_interrupt_button" onClick={handleRest}>Abbrechen</button>
-                                <button type="submit" className="base_button apply_submit_button">Bewerbung einreichen</button>
+                                <button type="reset" className="apply_interrupt_button" onClick={handleRest}>{t('apply.reset_button')}</button>
+                                <button type="submit" className="base_button apply_submit_button">{t('apply.submit_button')}</button>
                             </div>
-                            
                         </form>
                     </div>
                 </div>
