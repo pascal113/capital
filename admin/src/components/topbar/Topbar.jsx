0ffcf96b69aefffe616c81a2e7ee9b42d5485d66
damaomao84 <13324153861@163.com>
2023-05-20 19:03:51 +0800

    admin-dashboard-user

--- admin/src/components/topbar/Topbar.jsx
+++ admin/src/components/topbar/Topbar.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 import "./topbar.css";
-import { NotificationsNone, Language, Settings } from "@material-ui/icons";
+import IconButton from '@mui/material/IconButton';
+import Divider from '@mui/material/Divider';
+import Paper from '@mui/material/Paper';
+import MenuList from '@mui/material/MenuList';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Typography from '@mui/material/Typography';
+import ContentCut from '@mui/icons-material/ContentCut';
+import ContentCopy from '@mui/icons-material/ContentCopy';
+import ContentPaste from '@mui/icons-material/ContentPaste';
+import Cloud from '@mui/icons-material/Cloud';
+import { NotificationsNone, Language } from "@material-ui/icons";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import LogoutIcon from '@mui/icons-material/Logout';
+import VpnKeyIcon from '@mui/icons-material/VpnKey';
 
 export default function Topbar() {
   return (
@@ -11,13 +26,32 @@ export default function Topbar() {
         </div>
         <div className="topRight">
           <div className="topbarIconContainer">
-            <Language />
-            
+            <Language fontSize="large"/>
           </div>
           <div className="topbarIconContainer">
-            <Settings />
+            <IconButton aria-label="add an alarm">
+            <AccountCircleIcon fontSize="large" />
+            </IconButton>
+            <div className="topDropdownmenu">
+              <Paper sx={{ width: 320, maxWidth: '100%' }}>
+              <MenuList>
+                <MenuItem>
+                  <ListItemIcon>
+                    <VpnKeyIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>Change Password</ListItemText>
+                </MenuItem>
+                <Divider />
+                <MenuItem>
+                  <ListItemIcon>
+                    <LogoutIcon fontSize="small" />
+                  </ListItemIcon>
+                  <ListItemText>Logout</ListItemText>
+                </MenuItem>
+              </MenuList>
+            </Paper>
+            </div>
           </div>
-          <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" />
         </div>
       </div>
     </div>
